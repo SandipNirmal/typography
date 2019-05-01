bbe66faba78de84d368d8818b0226c26163b9ea6
Sandip Nirmal <sandipnirmal@icloud.com>
2019-05-01 22:47:50 +0530

    Added material typography

---
+++ packages/material/src/index.js
@@ -0,0 +1,86 @@
+import React from 'react'
+
+import './index.css'
+
+export const Typography = ({variant, children, ...other}) => {
+  let Element = 'p'
+  let className = 'body1'
+
+  switch (variant) {
+    case 'h1':
+      Element = 'h1'
+      className = 'h1'
+      break;
+
+    case 'h2':
+      Element = 'h2'
+      className = 'h2'
+      break;
+
+    case 'h3':
+      Element = 'h3'
+      className = 'h3'
+      break;
+
+    case 'h4':
+      Element = 'h4'
+      className = 'h4'
+      break;
+
+    case 'h5':
+      Element = 'h5'
+      className = 'h5'
+      break;
+
+    case 'h6':
+      Element = 'h6'
+      className = 'h6'
+      break;
+
+    case 'subtitle1':
+      Element = 'h6'
+      className = 'subtitle1'
+      break;
+
+    case 'subtitle2':
+      Element = 'h6'
+      className = 'subtitle2'
+      break;
+    
+    case 'subtitle3':
+      Element = 'h6'
+      className = 'subtitle3'
+      break;
+
+    case 'body1':
+      Element = 'p'
+      className = 'body1'
+      break;
+
+    case 'body2':
+      Element = 'p'
+      className = 'body2'
+      break;
+
+    case 'button':
+      Element = 'span'
+      className = 'button'
+      break;
+
+    case 'caption':
+      Element = 'span'
+      className = 'caption'
+      break;
+
+    case 'overline':
+      Element = 'span'
+      className = 'overline'
+      break;
+
+    default:
+      Element = 'p'
+      className = 'body1'
+  }
+
+  return <Element className={className} {...other}>{children}</Element>
+}
---
+++ packages/material/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
